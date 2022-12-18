44dfb8943840215212d9361be14848bb11c981c5
Abdullah <112586465+Abdullahtahirr@users.noreply.github.com>
2022-12-18 23:18:39 +0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,102 @@
+
+let products = [
+  {
+    Id: 1,
+    Name: "Ballpoint Pen",
+    Brand : "Dollar",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Pen",
+    Price : "$2.<small>99</small> <del class='fs-sm text-muted'>$3.<small>99</small></del>",
+    cartPrice : 2.99,
+    img: "assest/images/p-1.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>Sale</span>"
+  },
+  {
+    Id: 2,
+    Name: "Mechanical pencil 0.5mm",
+    Brand : "Piano",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Pencil",
+    Price : "$25.<small>59</small> <del class='fs-sm text-muted'> $36.<small>99</small></del>",
+    cartPrice : 25.59,
+    img: "assest/images/p-2.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>Sale</span>"
+  },
+  {
+    Id: 3,
+    Name: "Triangular color pencil",
+    Brand : "Winfun",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Pencil",
+    Price : "$5.<small>99</small> <del class='fs-sm text-muted'> $9.<small>99</small></del>",
+    cartPrice : 5.99,
+    img : "assest/images/p-3.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>Sale</span>"
+  },
+  {
+    Id: 4,
+    Name: "Camlin pencil",
+    Brand : "Winfun",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Pencil",
+    Price : "$7.<small>97</small> <del class='fs-sm text-muted'>$15.<small>99</small></del>",
+    cartPrice : 7.97,
+    img: "assest/images/p-4.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>Sale</span>"
+  },
+  {
+    Id: 5,
+    Name: "Box Sealing Tape",
+    Brand : "Dollar",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Taps",
+    Price : "$9.<small>29</small>",
+    cartPrice : 9.29,
+    img: "assest/images/p-5.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>New</span>"
+  },
+  {
+    Id: 6,
+    Name: "Leather Wallets Card",
+    Brand : "Mungyo",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Card",
+    Price : "$22.<small>69</small>",
+    cartPrice : 22.69,
+    img: "assest/images/p-6.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>New</span>"
+  },
+  {
+    Id: 7,
+    Name: "Gold Desk Accessories",
+    Brand : "Piano",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Box",
+    Price : "$19.<small>39</small>",
+    cartPrice : 19.39,
+    img: "assest/images/p-7.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>New</span>"
+  },
+  {
+    Id: 8,
+    Name: "colorful sticky pin",
+    Brand : "Piano",
+    Ratting : '<p class="text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>',
+    incart : 0,
+    Category: "Pins",
+    Price : "$4.<small>55</small>",
+    cartPrice : 4.55,
+    img: "assest/images/p-8.webp",
+    sale: "<span class='badge bg-danger badge-shadow'>New</span>"
+  }
+];
+
+          
+
